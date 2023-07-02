1628431f300fc284bb5a94441a34c9f27acb0ce3
UGEcko <vanhelsing314@outlook.com>
2023-07-02 10:50:19 -0400

    Error invalid arrays, fixed double-write on log file
    * Added try-catch to fix invalid arrays

--- objectToTrack.ts
+++ objectToTrack.ts
@@ -1,6 +1,6 @@
 // deno-lint-ignore-file no-explicit-any
-import { CustomEvent, optimizeAnimation, OptimizeSettings, parseFilePath, KeyframesVec3 } from "https://deno.land/x/remapper@3.1.1/src/mod.ts" 
-import { writeJsonSync, readJsonSync, } from "https://deno.land/std@0.66.0/fs/mod.ts";
+import { CustomEvent, optimizeAnimation, OptimizeSettings, parseFilePath, KeyframesVec3} from "https://deno.land/x/remapper@3.1.1/src/mod.ts" 
+import { writeJsonSync, readJsonSync } from "https://deno.land/std@0.66.0/fs/mod.ts";
 
 
 interface ObjectData {
@@ -10,6 +10,9 @@ interface ObjectData {
     color:  KeyframesVec3[];
     track?: string;
   }
+interface Item {
+  property: string;
+}
 
 enum returnProperties {
   "Position",
@@ -18,27 +21,32 @@ enum returnProperties {
 const optimSettings = new OptimizeSettings()
 optimSettings.passes = 5
 optimSettings.active = true
-optimSettings.performance_log = false
+optimSettings.performance_log = true
+optimSettings.optimizeSimilarPoints.active = true
 
 const Prop = returnProperties
 function processArray(filePath: string, track:string, returnProp: returnProperties, logObjectToFile?:string) {
-  const inputPath = parseFilePath(filePath, ".rmmodel").path; // Thanks swifter this helped lots :)
+
+  try {
+    const inputPath = parseFilePath(filePath, ".rmmodel").path; // Thanks swifter this helped lots :)
   const fileContent:any = readJsonSync(inputPath)
   const filteredObjects = fileContent.objects.filter((obj: ObjectData) => obj.track === track);
-  const optimizedPos: KeyframesVec3 = optimizeAnimation(filteredObjects.map((obj: ObjectData) => obj.pos), optimSettings);
-  const optimizedRot: KeyframesVec3 = optimizeAnimation(filteredObjects.map((obj: ObjectData) => obj.rot), optimSettings);
-  const [animatedPos]: KeyframesVec3 = optimizedPos; // Extract the animated position array
-  const [animatedRot]: KeyframesVec3 = optimizedRot; // Extract the animated position array
-        
+  const [optimizedPos]: KeyframesVec3 = optimizeAnimation(filteredObjects.map((obj: ObjectData) => obj.pos), optimSettings);
+  const [optimizedRot]: KeyframesVec3 = optimizeAnimation(filteredObjects.map((obj: ObjectData) => obj.rot), optimSettings);
+
         if(logObjectToFile) {
           writeJsonSync(logObjectToFile,filteredObjects) //Does NOT write the optimized keyframes to the file, only the object when its first found
           console.log(`Written object to ${logObjectToFile}`)
         }
-        if(returnProp == Prop.Position) {
-          return animatedPos
-        } else if (returnProp == Prop.Rotation) {
-          return animatedRot
-        }
+          if(returnProp == Prop.Position) {
+            return optimizedPos
+            } else if (returnProp == Prop.Rotation) {
+              return optimizedRot
+            }
+
+  } catch(error) {
+    console.error(`objectToTrack PROCESSARRAY ERROR: ${error}`)
+  }
 }
 
 function validateRmmodel(fileName: string): boolean {
@@ -50,7 +58,6 @@ function validateRmmodel(fileName: string): boolean {
   }
 }
 
-
 /**
  * @param  {string} envFile rmmodel of the environment
  * @param  {number} time time of the environment
@@ -61,10 +68,10 @@ function validateRmmodel(fileName: string): boolean {
  */
 export function objectToTrack(envFile: string, time: number, durationOffset: number, track: string, gameTrack:string, logObjectToFile?:string) {
 if (validateRmmodel(envFile) == true) {
-  const fuckass = new CustomEvent(time).animateTrack(gameTrack,durationOffset)
-  fuckass.animate.position = processArray(envFile,track, Prop.Position,logObjectToFile)
-  fuckass.animate.rotation = processArray(envFile,track,Prop.Rotation,logObjectToFile)
-  fuckass.push()
+  const f = new CustomEvent(time).animateTrack(gameTrack,durationOffset)
+  f.animate.position = processArray(envFile,track, Prop.Position,logObjectToFile)
+  f.animate.rotation = processArray(envFile,track,Prop.Rotation)
+  f.push()
 } else {
   console.error(`Invalid file. Expected .rmmodel`)
 }
