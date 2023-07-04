6cec6a20c0fc8e190f0c572577a93926afa0a5ee
UGEcko <vanhelsing314@outlook.com>
2023-07-04 17:22:40 -0400

    Update external.ts
    forgot!

--- module/external.ts
+++ module/external.ts
@@ -1,3 +1,6 @@
+import {parseFilePath, KeyframesVec3} from "https://deno.land/x/remapper@3.1.1/src/mod.ts" 
+import { writeJsonSync, readJsonSync } from "https://deno.land/std@0.66.0/fs/mod.ts";
+
 export function validateRmmodel(fileName: string): boolean {
   const inputPath = parseFilePath(fileName, ".rmmodel").path; // Thanks swifter this helped lots :)
   if(`${fileName}.rmmodel` == inputPath || fileName == inputPath) { // Support input of both example.rmmodel and just example.
